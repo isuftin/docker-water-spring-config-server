478c3a6ec19607a34b4321d968d6a560a8bccbf2
Suftin, Ivan <isuftin@usgs.gov>
2018-07-13 10:31:57 -0500

    Added artifact validation to artifact pulling script

--- pull-from-artifactory.sh
+++ pull-from-artifactory.sh
@@ -14,5 +14,17 @@ fi
 resource_endpoint="${repo_url}/${uri_formatted_group}/${artifact}/${version}/${artifact}-${version}.jar"
 
 echo "fetch $resource_endpoint"
-curl -k -o $output -X GET "$resource_endpoint"
-echo "Artifact: $group.$artifact\nVersion: $version\nRetireved At: $(date)" >> artifact-metadata.txt
+curl -k -o $output -X GET "${resource_endpoint}"
+echo "Artifact: ${group}.${artifact}\nVersion: ${version}\nRetireved At: $(date)" >> artifact-metadata.txt
+curl -k -o $output.md5 -X GET "${resource_endpoint}.md5"
+artifact_md5=$(md5sum $output | awk '{ print $1 }')
+remote_md5=$(cat $output.md5)
+test $artifact_md5 == $remote_md5
+if [ $? -ne 0 ]; then
+  echo "A problem has occurred while downloading artifact from ${resource_endpoint}"
+  echo "Downloaded artifact MD5: ${artifact_md5}"
+  echo "Expected MD5: ${remote_md5}"
+  exit 1
+else
+  echo "Artifact retrieved from ${resource_endpoint} verified to be valid."
+fi
